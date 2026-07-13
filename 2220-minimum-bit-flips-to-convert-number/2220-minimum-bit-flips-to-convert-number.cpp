52d286c65652878ddf10e16d637071245f2beea7
divyanshur0011 <divyanshuraidm7777@gmail.com>
2026-07-13 21:16:28 +0530

    Time: 0 ms (100%), Space: 8.1 MB (26.4%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -6,31 +6,14 @@ public:
 
         while (start>0 || goal>0)
         {
-            if (start%2==1 && goal%2==1)
+            if ((start%2)!=(goal%2))
             {
+                cnt++;
                 
-                start=(start-1)/2;
-                goal=(goal-1)/2;
+            }
+            start/=2;
+            goal/=2;
 
-            }
-            else if (start%2==0 && goal%2==0)
-            {
-                
-                start/=2;
-                goal/=2;
-            }
-            else if (start%2==0 && goal%2==1)
-            {
-                cnt++;
-                start/=2;
-                goal=(goal-1)/2;
-            }
-            else if (start%2==1 && goal%2==0)
-            {
-                cnt++;
-                start=(start-1)/2;
-                goal/=2;
-            }
         }
         return cnt;
         
